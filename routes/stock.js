fc66188120d925304c0b57f04d603fc6fbf9b81f
WilsonChen40172 <40795429+WilsonChen40172@users.noreply.github.com>
2022-09-13 18:01:55 +0800

    stock practice

--- routes/stock.js
+++ routes/stock.js
@@ -2,30 +2,35 @@ var express = require("express");
 var router = express.Router();
 var cheerio = require("cheerio");
 var request = require("request");
-const iconv = require("iconv-lite");
+// const iconv = require("iconv-lite");
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
-
   request(
-    "https://tw.stock.yahoo.com/future/futures.html",
+    "https://tw.screener.finance.yahoo.net/future/aa03?fumr=futurefull",
     function (error, response, body) {
-
       const data = [];
-      const $ = cheerio.load(body); // 載入 body
-      const list = $("#ext-wrap .ext-big-tbl tbody");
-      for (let i = 0; i < list.length; i++) {
-        const title = list.eq(i).find("tr").html();
-        // const author = list.eq(i).find(".meta .author").text();
-        // const date = list.eq(i).find(".meta .date").text();
-        // const link = list.eq(i).find(".title a").attr("href");
 
-        data.push({ title });
+      const $ = cheerio.load(body); // 載入 body
+      const list = $("#ext-wrap").find("table");
+
+      let tableNumber = 0; // 找第幾個table
+      const title = list.eq(tableNumber).find("tbody tr");
+
+      for (let index = 0; index < title.length; index++) {
+        const element = title.eq(index).find("td");
+        for (let index = 0; index < element.length; index++) {
+          const tdElement = element.eq(index).html();
+          data.push({ tdElement });
+        }
       }
-      console.log(list);
+      res.send("respond with a resource" + list);
       console.log(data);
-      // console.log(iconv.decode(list, 'big5'));
+
+      //   for (let index = 0; index < data.length; index++) {
+      //     console.log(data.eq(index).find("tr"));
+      //   }
+      
     }
   );
 });
